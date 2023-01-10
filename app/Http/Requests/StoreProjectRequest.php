7b8e68b9a4e1bea0b81f751b8ce6ca4433d8f7cf
Yuri-Punzo <yuripunzo@hotmail.it>
2023-01-10 21:43:33 +0100

    added CU of CRUD

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -13,7 +13,7 @@ class StoreProjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|unique:projects,title|min:5|max:100',
+            'slug' => 'max:255',
+            'description' => 'nullable'
         ];
     }
 }
