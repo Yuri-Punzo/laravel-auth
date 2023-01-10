90876f935c7e7cb72a8891fdd33503b49cb7a439
Yuri-Punzo <yuripunzo@hotmail.it>
2023-01-10 19:59:05 +0100

    CRUD wip

--- database/migrations/2023_01_10_154643_create_projects_table.php
+++ database/migrations/2023_01_10_154643_create_projects_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title', 100)->unique();
             $table->string('slug');
-            $table->string('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
